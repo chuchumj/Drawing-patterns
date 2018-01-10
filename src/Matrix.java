5edcc60728eaf1bf4cbd6493a52d470062914ff2
chuchumj <janeibodje@gmail.com>
2018-01-09 21:36:18 -0500

    modified

--- src/Matrix.java
+++ src/Matrix.java
@@ -25,28 +25,6 @@ class Matrix
            k =  --k;
         }
     }
-
-//    static void draw(int width, int height){
-//        int k = height/2 ;
-//        for(int i = 0; i<  height; i++) {
-//
-//            for (int j = 0; j < width; j++) {
-//
-//                if ((height / 2 ) == i && (width / 2 ) == j) {
-//                    String ans = ((height / 2 ) == i && (width / 2 ) == j) ? "x" : "o";
-//                    System.out.print(ans);
-//                }else if((height / 2) != i || (width / 2) != j){
-//
-//                    String ans1 = ((height / 2 - k) == i && (width / 2 - k) == j || (height / 2 - k) == i && (width / 2 + k) == j || (height / 2 + k) == i && (width / 2 - k) == j || (height / 2 + k) == i && (width / 2 + k) == j)? "x":"o";
-//                    System.out.print(ans1);
-//                }
-//            }
-//            System.out.println();
-//            k = --k;
-//        }
-
-
-
     public static void main(String[] args)
     {
         Scanner scan = new Scanner(System.in);
